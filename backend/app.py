213eb4cde9cea0b5a9946ff9e19c57b9d699b4ab
Fidian Peralta Lopez <donyoh1@hotmail.com>
2026-03-05 23:29:46 -0500

    mejorando backend para despliegue

--- backend/app.py
+++ backend/app.py
@@ -24,5 +24,5 @@ app.register_blueprint(ordenes_bp)
 
 # 🔧 IMPORTANTE para entornos como Render
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 41539))
     app.run(host="0.0.0.0", port=port)
